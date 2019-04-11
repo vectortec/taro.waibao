17ccb29c96453c2c9d91f15194e68853bea3945b
caijiangxu <caijiangxu@hqjy.com>
2019-04-11 15:47:37 +0800

    [feat](课程列表页): 增加无列表数据的显示方案

--- src/pages/courseList/index.js
+++ src/pages/courseList/index.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: 蔡江旭
+ * @Description: 课程列表页，搜索页
+ * @props: 
+ * @event: 
+ * @LastEditors: 蔡江旭
+ * @Date: 2019-04-02 16:23:03
+ * @LastEditTime: 2019-04-11 15:28:55
+ */
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image } from '@tarojs/components'
 import classNames from 'classnames'
 // import { connect } from '@tarojs/redux'
 // import { add, minus, asyncAdd } from '@/actions/counter'
@@ -63,51 +72,53 @@ class CourseList extends Component {
         value: 3,
       }]
     ],
-    course: {
-      "activiteId": 3,
-      "activitePoint": 10000,
-      "activitePrice": 99,
-      "activitePublishStatus": 2,
-      "activiteStatus": 2,
-      "activiteType": 1,
-      "activiteTypeName": "积分",
-      "activityEndTime": "2019-04-09 00:00:00",
-      "activityRepertory": 72,
-      "basicBuyCount": 66,
-      "buyCount": 1,
-      "buyPrice": 0,
-      "courseId": 2007,
-      "coverPicture": "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/DDoDAwq8c8e6twaEvfAbBKrZuGBX.jpg",
-      "dynamicCoverType": 1,
-      "evaluateCount": 0,
-      "evaluatePoint": 0,
-      "goodsCategoryId": 8,
-      "goodsCategoryName": "",
-      "id": 16,
-      "introduction": "<p>课程简介：</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>本课程主要讲解会计初入职场：包括入职流程、快速上手财务工作、开启良好职场习惯。</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>课程特色：</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>本课程采用微课精讲和真景实操相结合形式讲解会计初入职场！</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>其中微课精讲：着重详细介绍会计初入职场相关知识点！</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>其中真景实操：采用全面拍摄现实工作场景以及工作内容来感受会计初入职场的真实工作体验！</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>本课程老师经验丰富，授课幽默生动，颜值爆表！</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>本课程只讲干货，并且轻松易懂，学会无压力！</p>",
-      "isAudition": false,
-      "isCollection": 0,
-      "isRecommend": 0,
-      "issueStatus": 1,
-      "issueTime": null,
-      "keyframes": ["http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/rKlAUaL66-ljcUd9G9Ok1RTyqUFl.jpg", "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/1xgowWbEqmB3bhGi-62p9aFLqUTI.jpg", "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/8Oh9N3NwyeQUpfrfIgtnKrN93f3w.jpg", "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/f1I1GTsWQujO6MVz0ZXiMoUjDRS8.jpg", "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/BrCoSoyRHYGBzswZX-r42VTl8km0.jpg", "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/wsdrUtuX7AAZJ8X0CEn4W3CHlcn0.jpg"],
-      "name": "“会计入门+出纳变形记”之初入职场技能",
-      "pointDescribe": "",
-      "price": 0,
-      "rootCategoryId": 5,
-      "rootCategorySort": 1,
-      "stockCount": 999999993,
-      "sysDate": null,
-      "teacherIds": "4",
-      "teacherName": "",
-      "teacherNames": "",
-      "teachers": [],
-      "totalBuyCount": 67,
-      "type": 2,
-      "validityDate": -1,
-      "videoUrl": "",
-      "weightValue": 0
-    },
+    courseList: [
+      {
+        "activiteId": 3,
+        "activitePoint": 10000,
+        "activitePrice": 99,
+        "activitePublishStatus": 2,
+        "activiteStatus": 2,
+        "activiteType": 1,
+        "activiteTypeName": "积分",
+        "activityEndTime": "2019-04-09 00:00:00",
+        "activityRepertory": 72,
+        "basicBuyCount": 66,
+        "buyCount": 1,
+        "buyPrice": 0,
+        "courseId": 2007,
+        "coverPicture": "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/DDoDAwq8c8e6twaEvfAbBKrZuGBX.jpg",
+        "dynamicCoverType": 1,
+        "evaluateCount": 0,
+        "evaluatePoint": 0,
+        "goodsCategoryId": 8,
+        "goodsCategoryName": "",
+        "id": 16,
+        "introduction": "<p>课程简介：</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>本课程主要讲解会计初入职场：包括入职流程、快速上手财务工作、开启良好职场习惯。</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>课程特色：</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>本课程采用微课精讲和真景实操相结合形式讲解会计初入职场！</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>其中微课精讲：着重详细介绍会计初入职场相关知识点！</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>其中真景实操：采用全面拍摄现实工作场景以及工作内容来感受会计初入职场的真实工作体验！</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>本课程老师经验丰富，授课幽默生动，颜值爆表！</p>\n<p><span lang=\"EN-US\">&nbsp;</span></p>\n<p>本课程只讲干货，并且轻松易懂，学会无压力！</p>",
+        "isAudition": false,
+        "isCollection": 0,
+        "isRecommend": 0,
+        "issueStatus": 1,
+        "issueTime": null,
+        "keyframes": ["http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/rKlAUaL66-ljcUd9G9Ok1RTyqUFl.jpg", "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/1xgowWbEqmB3bhGi-62p9aFLqUTI.jpg", "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/8Oh9N3NwyeQUpfrfIgtnKrN93f3w.jpg", "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/f1I1GTsWQujO6MVz0ZXiMoUjDRS8.jpg", "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/BrCoSoyRHYGBzswZX-r42VTl8km0.jpg", "http://hq-expert.oss-cn-shenzhen.aliyuncs.com/hangjia/image/wsdrUtuX7AAZJ8X0CEn4W3CHlcn0.jpg"],
+        "name": "“会计入门+出纳变形记”之初入职场技能",
+        "pointDescribe": "",
+        "price": 0,
+        "rootCategoryId": 5,
+        "rootCategorySort": 1,
+        "stockCount": 999999993,
+        "sysDate": null,
+        "teacherIds": "4",
+        "teacherName": "",
+        "teacherNames": "",
+        "teachers": [],
+        "totalBuyCount": 67,
+        "type": 2,
+        "validityDate": -1,
+        "videoUrl": "",
+        "weightValue": 0
+      }
+    ],
     searchRecentWord: [
       '商业手工帐',
       '商业电脑账',
@@ -173,6 +184,7 @@ class CourseList extends Component {
       searchInputValue,
       searchTipsVisible: true,
     })
+    // 请求后台拿提示
   }
 
   /**
@@ -202,6 +214,8 @@ class CourseList extends Component {
     this.setState({
       searchTipsVisible: false,
     })
+
+    // 请求后台拿数据
   }
 
   /**
@@ -219,11 +233,26 @@ class CourseList extends Component {
     })
   }
 
+  /**
+   * @Description: 点击看看其他课程
+   * @params: 
+   * @return: 
+   * @LastEditors: 蔡江旭
+   * @LastEditTime: Do not edit
+   * @Date: 2019-04-11 15:28:07
+   */
+  gotoOtherCourse = () => {
+    console.log('goto');
+    // Taro.redirectTo({
+    //   url: '/pages/courseList/index'
+    // })
+  }
+
   render () {
     const {
       sortOptions,
       filterOptions,
-      course,
+      courseList,
       searchRecentWord,
       searchInputFocus,
       searchTipsWord = [],
@@ -283,8 +312,26 @@ class CourseList extends Component {
           onChange={console.log}
         />
         {/* 课程列表 */}
-        <View className={styles.courseList}>
-          <CourseCard course={course} />
+        <View className={styles.listBox}>
+          {courseList.map((course, index) => (
+            <CourseCard course={course} key={index} />
+          ))}
+          {/* 无数据时 */}
+          {(!courseList || courseList.length === 0) &&
+            <View className={styles.emptyList}>
+              <View className={styles.imgBox}>
+                <Image
+                  className={styles.img}
+                  mode='widthFix'
+                  src='https://hq-expert-online-school.oss-cn-shenzhen.aliyuncs.com/demo_img/empty-shopcar.png'
+                />
+              </View>
+              <View>暂无相关搜索结果</View>
+              <View>
+                <Text className={styles.gotoOther} onClick={this.gotoOtherCourse}>看看其他课程</Text>
+              </View>
+            </View>
+          }
         </View>
       </View>
     )
