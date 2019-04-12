1ff515c4672416ccaa975f4e6dc97da6a87ec508
zgt <zhanggt2016@163.com>
2019-04-12 12:17:01 +0800

    支付--支付成功页面

--- src/pages/payment/index.js
+++ src/pages/payment/index.js
@@ -1,8 +1,9 @@
 import Taro, {Component} from '@tarojs/taro'
 import {View} from '@tarojs/components'
 import HeadBar from 'components/head-bar'
-import SubmitOrder from './submit-order'
-import PayResultWait from './pay-result-wait'
+// import SubmitOrder from './submit-order'
+// import PayResultWait from './pay-result-wait'
+import PayResultSuccess from './pay-result-success'
 import styles from './index.module.scss'
 
 export default class Payment extends Component {
@@ -14,8 +15,9 @@ export default class Payment extends Component {
     return (
       <View className={`${styles.payWrap}`}>
         <HeadBar />
-        <SubmitOrder />
+        {/* <SubmitOrder /> */}
           {/* <PayResultWait /> */}
+          <PayResultSuccess />
       </View>
     )
   }
