e2834d21c713c9d80ac4a2935f4d40982b48c1c2
lee <lee@sina.com>
2019-04-14 23:07:02 +0800

    修复代码

--- src/components/Vtable/index.js
+++ src/components/Vtable/index.js
@@ -4,33 +4,33 @@ import './index.scss'
 
 
 class SumbitOrder extends Taro.Component {
-  FormData2table() {
-    const keyDict = {}
-    const leng = this.props.title.length
-    this.props.title.map((item, index) => {
-      keyDict[item.key] = { ...item, index }
-    })
-    return this.props.formData.map(item => {
-      const Row = new Array(leng).fill(<View className="td">无数据</View>, 0)
-      // Row.fill('空', 0, leng)
-      for (let key in item) {
-        Row[keyDict[key].index] = <View className="td">{item[key]}</View>
-      }
-      return Row
-    })
-  }
   render() {
+    const FormData2table = () => {
+      const keyDict = {}
+      const leng = this.props.title.length
+      this.props.title.map((item, index) => {
+        keyDict[item.key] = { ...item, index }
+      })
+      return this.props.formData.map(item => {
+        const Row = new Array(leng).fill(<View className='td'>无数据</View>, 0)
+        // Row.fill('空', 0, leng)
+        for (let key in item) {
+          Row[keyDict[key].index] = <View className='td'>{item[key]}</View>
+        }
+        return Row
+      })
+    }
     return (
       <View className='statistics'>
-        <View className="container">
-          <View className="table">
-            <View className="tr">
+        <View className='container'>
+          <View className='table'>
+            <View className='tr'>
               {this.props.title.map(item => (
-                <View className="th">{item.label}</View>
+                <View className='th'>{item.label}</View>
               ))}
             </View>
-            <View className="tr">
-              {this.FormData2table.call(this)}
+            <View className='tr'>
+              {FormData2table.call(this)}
             </View>
           </View>
         </View>
