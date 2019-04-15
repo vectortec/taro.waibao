6b17ba73de2ca72ef4a712801be06a45c0bb1763
caijiangxu <caijiangxu@hqjy.com>
2019-04-15 14:20:33 +0800

    [feat](资讯详情页): 完成资讯详情页的UI

--- src/pages/newsDetail/index.js
+++ src/pages/newsDetail/index.js
@@ -1,13 +1,24 @@
+/*
+ * @Author: 蔡江旭
+ * @Description: 资讯详情页
+ * @props: 
+ * @event: 
+ * @LastEditors: 蔡江旭
+ * @Date: 2019-04-02 17:22:27
+ * @LastEditTime: 2019-04-15 09:50:53
+ */
+
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
-import { connect } from '@tarojs/redux'
-import { add, minus, asyncAdd } from '@/actions/counter'
+import classNames from 'classnames'
+// import { connect } from '@tarojs/redux'
+// import { add, minus, asyncAdd } from '@/actions/counter'
 import Loading from '@/components/loading'
 
 import styles from './index.module.scss'
 
 
-@connect(state => state.counter, {add, minus, asyncAdd})
+// @connect(state => state.counter, {add, minus, asyncAdd})
 class NewsDetail extends Component {
 
   config = {
@@ -15,7 +26,16 @@ class NewsDetail extends Component {
   }
 
   state = {
-    loading: true
+    loading: false,
+    shareIconList: [{
+      icon: 'wechat',
+    }, {
+      icon: 'qq',
+    }, {
+      icon: 'wefriend',
+    }, {
+      icon: 'weibo',
+    }]
   }
 
   // 慎用
@@ -26,19 +46,48 @@ class NewsDetail extends Component {
   componentDidMount() {
     console.log(Taro.getEnv())
     console.log(Taro.ENV_TYPE)
-    let timer = setTimeout(_ => {
-      this.setState({loading: false})
-      clearTimeout(timer)
-    }, 1000)
   }
   render () {
+    const { shareIconList } = this.state;
     if (this.state.loading) {
       return <Loading />
     }
 
     return (
-      <View className={styles.demo}>
-        <Text>NewsDetail page</Text>
+      <View className={styles.newsDetail}>
+        <View className={styles.title}>写给产品经理们|团队的重要性，会让改变世界的路上更加有趣</View>
+        <View className={styles.newsInfo}>
+          <Text className={styles.date}>11.14</Text>
+          <Text className={styles.time}>16:24</Text>
+          <Text className={styles.viewNum}>
+            <Text
+              className={classNames(styles.icon, 'iconfont', 'iconchakankanziliao')}
+            ></Text>
+            123
+          </Text>
+        </View>
+        <Text>CourseDetail page</Text>
+        {/* 点赞 */}
+        <View className={styles.likeBox}>
+          <View className={styles.likeBtn}>
+            <Text>点赞</Text>
+          </View>
+        </View>
+        {/* 分享 */}
+        <View className={styles.shareBox}>
+          {/* 标题 */}
+          <View className={styles.shareTitle}>
+            <View className={styles.titleTextBox}>
+              <Text className={styles.titleText}>分享至</Text>
+            </View>
+          </View>
+          {/* icon */}
+          <View className={styles.shareList}>
+            {shareIconList.map((ele, index) => (
+              <View className={classNames(styles.shareBtn, ele.icon)} key={index}>{ele.icon}</View>
+            ))}
+          </View>
+        </View>
       </View>
     )
   }
