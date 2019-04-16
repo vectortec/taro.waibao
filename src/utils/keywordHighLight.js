54213471831806636092e2b157f1b79e3b1e8a31
caijiangxu <caijiangxu@hqjy.com>
2019-04-16 18:32:31 +0800

    [feat]: 高亮函数增加回调的使用方式

--- src/utils/keywordHighLight.js
+++ src/utils/keywordHighLight.js
@@ -1,16 +1,35 @@
 /**
- * @Description: 
- * @params: 
+ * @Description: 字符串高亮处理函数
+ * @params {String} originText 原来文本
+ * @params {String} keyword 高亮关键字
+ * @params {String | Function} highLightTemplate 高亮结果html // <span>{{{text}}}</span> {{{text}}}目前是固定
  * @return: 
  * @LastEditors: 蔡江旭
  * @LastEditTime: Do not edit
  * @Date: 2019-04-15 15:38:17
  */
+
 export default function keywordHighLight (originText = '', keyword, highLightTemplate = '') {
     const keywordRegExp = new RegExp(keyword, 'g');
-    return originText.replace(keywordRegExp, function (match) {
-        const tempRegExp = /{{{text}}}/g;
-        const hasTempStr = tempRegExp.test(highLightTemplate);
-        return hasTempStr ? highLightTemplate.replace(tempRegExp, match) : match;
-    })
+    if (typeof highLightTemplate === 'function') {
+        let curCharStart = 0;
+        let curCharLen = 0;
+        const result = originText.split(keywordRegExp);
+
+        // Apply fn to all odd elements
+        for (let i = 1, length = result.length; i < length; i += 2) {
+            curCharLen = result[i].length;
+            curCharStart += result[i - 1].length;
+            result.splice(i, 0, highLightTemplate(keyword, i, curCharStart));
+            curCharStart += curCharLen;
+        }
+
+        return result;
+    } else {
+        return originText.replace(keywordRegExp, function (match) {
+            const tempRegExp = /{{{text}}}/g;
+            const hasTempStr = tempRegExp.test(highLightTemplate);
+            return hasTempStr ? highLightTemplate.replace(tempRegExp, match) : match;
+        });
+    }
 }
